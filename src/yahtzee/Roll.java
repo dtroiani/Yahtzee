6f0198b1ecdbaadfe9c25a7dd575fc0549293cd3
dtroiani <dtroiani@danielet-PC.adrialab.intra>
2015-08-07 16:24:03 +0200

    Estratto codice per generare rappresentazione canonica del risultato del lancio dei dati

--- src/yahtzee/Roll.java
+++ src/yahtzee/Roll.java
@@ -21,5 +21,22 @@ public class Roll {
         dice[3] = dice4;
         dice[4] = dice5;
     }
+    
+    /**
+    * Returns a canonical representation of the dice roll.
+    * The representation consists of a integer array C[] where C[i] is equal to the sum of the dice occurrences with rank i.
+    * For example, for a roll(2, 4, 3, 2, 4) the result is C[1]=0, C2=[2], C[3]=1, C[4]=2, C[5]=0, C[6]=0
+    * N.B. Ignore the element with index 0
+    * @return array of integer C where C[i] is the number of occurrences of dice with rank equal to i
+    */
+    public int[] getCanonicalRepresentation() {
+        int countInstance[] = new int[7];
+        
+        for (int i = 0; i < dice.length; i++) {
+            countInstance[dice[i]] = countInstance[dice[i]] + 1;
+        }
+        
+        return countInstance;
+    }
  
 }
