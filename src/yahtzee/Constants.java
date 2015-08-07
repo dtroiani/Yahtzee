873481861790331b53bcd44839cbfbb96fc2102d
dtroiani <dtroiani@danielet-PC.adrialab.intra>
2015-08-07 16:23:11 +0200

    Estratte alcune costanti

---
+++ src/yahtzee/Constants.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yahtzee;
+
+/**
+ *
+ * @author dtroiani
+ */
+public class Constants {
+
+    public static final int DICE_ONE = 1;
+    public static final int DICE_TWO = 2;
+    public static final int DICE_THREE = 3;
+    public static final int DICE_FOUR = 4;
+    public static final int DICE_FIVE = 5;
+    public static final int DICE_SIX = 6;
+    
+    public static final int SCORE_SMALLSTRAIGHT = 30;
+    public static final int SCORE_LARGESTRAIGHT = 40;
+    public static final int SCORE_FULLHOUSE = 25;
+    public static final int SCORE_YAHTZEE = 50;
+}
